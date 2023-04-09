d66258e2809785737bd438643f6e0c1dbf95f4df
Pronoy <pronoydas463@gmail.com>
2023-04-10 01:33:23 +0600

    Header section finished with responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,18 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header-container">
-      <h1>header</h1>
+      <div className="header-text">
+        <h1>Getting one step nearer to your dream job</h1>
+        <p>
+          With all the information you require, explore many career options. It
+          is your future. Find it here. Manage each step of the employment
+          application process.
+        </p>
+        <button className="btnGetStarted">Get Started</button>
+      </div>
+      <div className="header-image">
+        <img src={logo} alt="image" />
+      </div>
     </div>
   );
 };
